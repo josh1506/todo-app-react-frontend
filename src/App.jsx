84384982aa840ca8b27f0b1c37f2b4255a156803
josh1506 <jmjabor@gmail.com>
2021-03-29 17:08:34 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
+
+function App(props) {
+    return (
+        <div>
+            <Switch>
+                <Route />
+                <Redirect />
+            </Switch>
+        </div>
+    );
+}
+
+export default App;
