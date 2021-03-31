31e544c363f6bba7afb4d1af70e1282071eb173d
josh1506 <jmjabor@gmail.com>
2021-03-31 15:00:13 +0800

    Create ForgotPassword.jsx

---
+++ src/components/pages/ForgotPassword.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+import '../assets/css/forgotpassword.css'
+
+
+function ForgotPassword(props) {
+    return (
+        <div className='forgotpass-page-container'>
+            <div className="forgotpass-container">
+                <p className='forgotpass-paragraph'>If you've lost your password or wish to reset it, input your recovery email to send for the next step.</p>
+                <label className='forgotpass-label' for='forgot-pass-email'>Email</label>
+                <input className='forgotpass-input-text' type="text" name="forgot-pass-email" id="forgot-pass-email" placeholder='Enter Email' />
+                <div className='forgotpass-btn-container'>
+                    <Link to='/login' className='forgotpass-btn-cancel'>Cancel</Link>
+                    <button className='forgotpass-btn-send'>Send</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ForgotPassword;
