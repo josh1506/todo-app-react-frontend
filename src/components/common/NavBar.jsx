cd8c0c3e2760a9d5e44f6a23bc94830d02037497
josh1506 <jmjabor@gmail.com>
2021-04-01 21:28:49 +0800

    Update NavBar.jsx

--- src/components/common/NavBar.jsx
+++ src/components/common/NavBar.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 
+import '../assets/css/navbar.css'
+
+
 function NavBar(props) {
     return (
-        <div className=''>
-            <h1>Todo App</h1>
-            <div>
-                <Link>Login</Link>
-                /
-                <Link>Sign up</Link>
+        <div className='navbar-container'>
+            <div></div>
+            <Link to='/' className='navbar-title'>Todo App</Link>
+            <div className='navbar-button-container'>
+                <Link to='/login' className='navbar-button'>Login</Link>
+                <p className='navbar-button-seperator'>/</p>
+                <Link to='/signup' className='navbar-button'>Signup</Link>
             </div>
         </div>
     );
