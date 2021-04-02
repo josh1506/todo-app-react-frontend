896362e48f3e2fcd0881b7091bb9b776cb80c508
josh1506 <jmjabor@gmail.com>
2021-04-02 21:36:27 +0800

    Update LandingPage.jsx

--- src/components/pages/LandingPage.jsx
+++ src/components/pages/LandingPage.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
+
+import '../assets/css/landingpage.css'
 
 function LandingPage(props) {
     return (
-        <div>
-
+        <div className="landing-page-container">
+            <div className="landing-page-show-content">
+                <div className="landing-page-img"></div>
+                <Link to='/login' className="landing-page-button">Register/Login</Link>
+            </div>
+            <div className='landing-page-paragraph-container'>
+                <p className="landing-page-paragraph">To-do lists should not take hours to build and should not be too complex. Any second you spent making a task list and planning for the job ahead is time lost instead of going out there and removing tasks from your calendar.</p>
+            </div>
         </div>
     );
 }
