d78fc61014d41edc879946f19f487d6596f84ca0
josh1506 <jmjabor@gmail.com>
2021-04-03 15:20:08 +0800

    Create route

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 
+import LandingPage from './components/pages/LandingPage';
+import Login from './components/pages/Login';
+import NavBar from './components/common/NavBar';
+import Signup from './components/pages/Signup';
+import ToDo from './components/pages/ToDo';
+import './components/assets/css/app.css';
+import ForgotPassword from './components/pages/ForgotPassword';
+import Footer from './components/common/Footer';
+
 function App(props) {
     return (
         <div>
-            <Switch>
-                <Route />
-                <Redirect />
-            </Switch>
+            <NavBar />
+            <div className="app-content-container">
+                <Switch>
+                    <Route path='/todo' component={ToDo} />
+                    <Route path='/forgot-password' component={ForgotPassword} />
+                    <Route path='/login' component={Login} />
+                    <Route path='/signup' component={Signup} />
+                    <Route path='/' component={LandingPage} />
+                    <Redirect />
+                </Switch>
+            </div>
+            <Footer />
         </div>
     );
 }
