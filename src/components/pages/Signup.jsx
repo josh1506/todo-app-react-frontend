caa72d96835eb92391f869c554c6850fd707d244
josh1506 <jmjabor@gmail.com>
2021-04-05 09:52:29 +0800

    Add user verification in signup page

--- src/components/pages/Signup.jsx
+++ src/components/pages/Signup.jsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom'
 
 import '../assets/css/signup.css'
 
 function Signup(props) {
+    useEffect(() => {
+        const token = props.auth || localStorage.getItem('auth_token')
+        if (token) props.history.replace('/todo')
+    }, [])
+
     return (
         <div className='signup-page-container'>
             <div className='signup-container'>
                 <form action="" className="signup-form-container">
-                    <label htmlFor="username" className='signup-label'>Username/Email</label>
-                    <input type="text" name="username" id="username" placeholder="Username/Email" className='signup-input-text' />
+                    <label htmlFor="username" className='signup-label'>Username</label>
+                    <input type="text" name="username" id="username" placeholder="Username" className='signup-input-text' />
                     <label htmlFor="password" className='signup-label'>Password</label>
                     <input type="password" name="password" id="password" className='signup-input-text' placeholder='Password' />
                     <label htmlFor="confirm-password" className='signup-label'>Confirm Password</label>
