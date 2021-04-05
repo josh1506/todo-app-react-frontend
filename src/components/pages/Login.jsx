272ae3716ab2ae8ae35bb467c0bf0a7dd4b8f76c
josh1506 <jmjabor@gmail.com>
2021-04-05 09:51:39 +0800

    Add authentication

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,20 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 
+import { authAdd } from '../../app/auth'
+import route from '../../app/route'
 import '../assets/css/login.css'
 
 function Login(props) {
+    const [userData, setUserData] = useState({ username: '', password: '' })
+    const [error, setError] = useState('')
+
+    const handleSubmit = async () => {
+        console.log('click')
+        const data = await route.post('auth/', userData)
+            .catch(() => setError('username or password is incorrect'))
+
+        if (data) {
+            setError('')
+            const token = data.data.token
+            localStorage.setItem('auth_token', token)
+            props.authAdd(token)
+            props.history.replace('/todo')
+        }
+    }
+
+    useEffect(() => {
+        const token = props.auth || localStorage.getItem('auth_token')
+        if (token) props.history.replace('/todo')
+    }, [])
+
     return (
         <div className="login-page-container">
             <div className='login-container'>
-                <form className='login-form-container'>
-                    <label htmlFor="username" className="login-label">Username/Email</label>
-                    <input type="text" name="username" id="username" className="login-input-text" placeholder="Username/Email" />
+                {error && <p className="login-error-label">* {error}</p>}
+                <form className='login-form-container' onSubmit={handleSubmit}>
+                    <label htmlFor="username" className="login-label">Username</label>
+                    <input
+                        type="text"
+                        name="username"
+                        id="username"
+                        className="login-input-text"
+                        placeholder="Username"
+                        value={userData.username}
+                        onChange={e => setUserData({ ...userData, username: e.target.value })}
+                    />
                     <label htmlFor="password" className="login-label">Password</label>
-                    <input type="text" name="password" id="password" className="login-input-text" placeholder="Password" />
+                    <input
+                        type="text"
+                        name="password"
+                        id="password"
+                        className="login-input-text"
+                        placeholder="Password"
+                        value={userData.password}
+                        onChange={e => setUserData({ ...userData, password: e.target.value })}
+                    />
                 </form>
                 <div className='login-btn-container'>
-                    <button className='login-btn-login'>Login</button>
+                    <button className='login-btn-login' onClick={() => handleSubmit()}>Login</button>
                     <div className='login-btn-container-two'>
                         <Link to='/forgot-password' className='login-btn-forgotpass'>Forgot Password</Link>
                         <Link to='/signup' className='login-btn-signup'>Signup</Link>
@@ -26,4 +68,9 @@ function Login(props) {
     );
 }
 
-export default Login;
+
+const mapStateToProps = state => {
+    return { auth: state.auth }
+}
+
+export default connect(mapStateToProps, { authAdd })(Login);
