8062cd049f4d448ec86f896379c7aeabde34183a
josh1506 <jmjabor@gmail.com>
2021-04-05 09:48:29 +0800

    Move class from anchor to font awesome icon

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -10,11 +10,11 @@ function Footer(props) {
         <div className='footer-container'>
             <p className='footer-text'>&copy; Copyright 2021, <a className='footer-link' href="mailto:jmjabor@gmail.com">Joshua Michael Jabor</a></p>
             <div className='footer-icon-container'>
-                <a href='https://www.linkedin.com/in/joshua-michael-jabor/' className='footer-icon' target="_blank">
-                    <FontAwesomeIcon icon={faLinkedin} color='white' size='lg' />
+                <a href='https://www.linkedin.com/in/joshua-michael-jabor/' target="_blank">
+                    <FontAwesomeIcon icon={faLinkedin} color='white' size='lg' className='footer-icon' />
                 </a>
-                <a href='https://github.com/josh1506' className='footer-icon' target="_blank">
-                    <FontAwesomeIcon icon={faGithub} color='white' size='lg' />
+                <a href='https://github.com/josh1506' target="_blank">
+                    <FontAwesomeIcon icon={faGithub} color='white' size='lg' className='footer-icon' />
                 </a>
             </div>
         </div>
