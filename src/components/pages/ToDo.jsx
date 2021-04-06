cafcd9d3da0d8c1fcb5d73d73ffc18706deb5134
josh1506 <jmjabor@gmail.com>
2021-04-06 21:56:26 +0800

    Add modal for create, update and delete. Calculating task status also updated

--- src/components/pages/ToDo.jsx
+++ src/components/pages/ToDo.jsx
@@ -1,11 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlusCircle, faEdit } from '@fortawesome/free-solid-svg-icons'
 
-import { todoGetData } from '../../app/todo'
+import todo, { todoGetData, todoUpdated, taskUpdated } from '../../app/todo'
+import TodoCreateModal from '../common/TodoCreateModal';
 import '../assets/css/todo.css'
+import TodoUpdateModal from './../common/TodoUpdateModal';
 
 function ToDo(props) {
     const [selectedItem, setSelectedItem] = useState('')
+    const [todoSelected, setTodoSelected] = useState({})
+    const [createModal, setCreateModal] = useState(false)
+    const [updateModal, setUpdateModal] = useState(false)
 
     useEffect(() => {
         const token = props.auth || localStorage.getItem('auth_token')
@@ -14,43 +21,96 @@ function ToDo(props) {
         props.todoGetData()
     }, [])
 
-    const calcTotalTask = (task) => {
-        if (task.length <= 0) return 0
+    const calcTotalTask = (todo) => {
+        if (todo.status) return 'Done'
+        if (!todo.checklist) return '0%'
+        if (todo.checklist.length <= 0) return '0%'
 
         let completedTask = 0
-        const totalTask = task.length
-        task.map(taskItem => taskItem.completed === true ? completedTask += 1 : completedTask)
-        return Math.ceil((completedTask / totalTask) * 100)
+        const totalTask = todo.checklist.length
+        todo.checklist.map(taskItem => taskItem.status === true ? completedTask += 1 : completedTask)
+        const totalStatus = Math.ceil((completedTask / totalTask) * 100)
+
+        if (totalStatus >= 100) return 'Done'
+
+        return `${totalStatus}%`
     }
 
-    if (props.todoList <= 0) return (
+    const handleTodoStatus = (todo) => {
+        props.todoUpdated(todo)
+    }
+
+    const handleTaskStatus = (todoID, task) => {
+        props.taskUpdated(todoID, task)
+    }
+
+    const sortDataByStatus = (data) => {
+        return data.sort((a, b) => { return a.status - b.status })
+    }
+
+    if (props.todoList <= 0 || !props.todoList) return (
         <div className='todo-page-container'>
             <div className='todo-container'>
+                <div className="todo-add-icon-container">
+                    <FontAwesomeIcon icon={faPlusCircle} color='white' size='lg' className='todo-add-icon' onClick={() => setCreateModal(true)} />
+                </div>
                 <h1 className='todo-item-title' style={{ textAlign: 'center' }}>There are no to-do created</h1>
             </div>
+
+            {!createModal ? '' : <TodoCreateModal onSetModal={setCreateModal} />}
+            {!updateModal ? '' : <TodoUpdateModal onSetModal={setUpdateModal} itemSelect={todoSelected} />}
         </div >
     )
 
     return (
         <div className='todo-page-container'>
             <div className='todo-container'>
-                {props.todoList.map(todo =>
+                <div className="todo-add-icon-container">
+                    <FontAwesomeIcon icon={faPlusCircle} color='white' size='lg' className='todo-add-icon' onClick={() => setCreateModal(true)} />
+                </div>
+                {sortDataByStatus(props.todoList).map(todo =>
                     <div className={`todo-item ${selectedItem === todo.id ? 'active' : ''}`} key={todo.id}>
                         <div className='todo-item-title-container' onClick={() => selectedItem === todo.id ? setSelectedItem('') : setSelectedItem(todo.id)}>
-                            <h1 className='todo-item-title'>{todo.memo}</h1>
-                            <p className='todo-item-title'>{calcTotalTask(todo.checklist)}%</p>
+                            <div className='item-title-container'>
+                                <label className="checkbox-container">
+                                    <input type="checkbox" checked={todo.status} onChange={() => handleTodoStatus(todo)} />
+                                    <span className="checkbox-checkmark"></span>
+                                </label>
+                                <h1 className='todo-item-title'>{todo.memo}</h1>
+                            </div>
+                            <div className='todo-status-container'>
+                                <p className='todo-item-title'>{calcTotalTask(todo)}</p>
+                                <FontAwesomeIcon
+                                    icon={faEdit}
+                                    color='white'
+                                    size='lg'
+                                    className='todo-add-icon'
+                                    onClick={() => {
+                                        setUpdateModal(true)
+                                        setTodoSelected(todo)
+                                    }} />
+                            </div>
                         </div>
                         <div className='todo-task-list'>
                             <ol>
-                                {todo.checklist.map(task =>
-                                    <li key={task.id}>{task.text}</li>
+                                {sortDataByStatus(todo.checklist).map(task =>
+                                    <li key={task.id} className='checklist-container'>
+                                        <label className="checkbox-container-mini">
+                                            <input type="checkbox" checked={task.status} onChange={() => handleTaskStatus(todo.id, task)} />
+                                            <span className="checkbox-checkmark-mini"></span>
+                                        </label>
+                                        {task.text}
+                                    </li>
                                 )}
                             </ol>
                         </div>
                     </div>
                 )}
             </div>
-        </div>
+
+            {!createModal ? '' : <TodoCreateModal onSetModal={setCreateModal} />}
+            {!updateModal ? '' : <TodoUpdateModal onSetModal={setUpdateModal} itemSelect={todoSelected} />}
+        </div >
     );
 }
 
@@ -58,4 +118,4 @@ function ToDo(props) {
 const mapStateToProps = state => {
     return { todoList: state.todo }
 }
-export default connect(mapStateToProps, { todoGetData })(ToDo);
+export default connect(mapStateToProps, { todoGetData, todoUpdated, taskUpdated })(ToDo);
