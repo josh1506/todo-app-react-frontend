d2bd298ad83b06f8b49a58f5448dc3b0200df5ea
josh1506 <jmjabor@gmail.com>
2021-04-06 21:58:31 +0800

    Update Footer.jsx

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
-import { Link } from 'react-router-dom'
 
 import '../assets/css/footer.css'
 
