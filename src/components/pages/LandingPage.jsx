6e6dc5632159c3460d6e410be7b8a1f2d0098e54
josh1506 <jmjabor@gmail.com>
2021-04-08 12:00:13 +0800

    Add go to dashboard button

--- src/components/pages/LandingPage.jsx
+++ src/components/pages/LandingPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom'
 
 import '../assets/css/landingpage.css'
@@ -8,7 +9,9 @@ function LandingPage(props) {
         <div className="landing-page-container">
             <div className="landing-page-show-content">
                 <div className="landing-page-img"></div>
-                <Link to='/login' className="landing-page-button">Register/Login</Link>
+                {props.auth ?
+                    <Link to='/todo' className="landing-page-button">Go to Dashboard</Link> :
+                    <Link to='/login' className="landing-page-button">Register/Login</Link>}
             </div>
             <div className='landing-page-paragraph-container'>
                 <p className="landing-page-paragraph">To-do lists should not take hours to build and should not be too complex. Any second you spent making a task list and planning for the job ahead is time lost instead of going out there and removing tasks from your calendar.</p>
@@ -17,4 +20,8 @@ function LandingPage(props) {
     );
 }
 
-export default LandingPage;
+const mapStateToProps = state => {
+    return { auth: state.auth }
+}
+
+export default connect(mapStateToProps)(LandingPage);
